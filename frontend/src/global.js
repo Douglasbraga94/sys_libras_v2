ad4e5d579342acf5a06ccc0e15b45601fa1a716a
Victor Cortes <victor.76467@gmail.com>
2022-04-21 21:35:49 -0300

    Retornando configuração

--- frontend/src/global.js
+++ frontend/src/global.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 
 export const userKey = '__knowledge_user'
-export const baseApiUrl = 'https://2a9b-2804-14c-6584-5180-ec54-f5d9-f9a1-837.ngrok.io'
+export const baseApiUrl = 'http://localhost:3000'
 
 export function showError(e) {
     if(e && e.response && e.response.data) {
-        Vue.toasted.global.defaultError({ msg : e.response.data })
+        Vue.toasted.global.defaultError({ msg : e.response
+                .data })
     } else if(typeof e === 'string') {
         Vue.toasted.global.defaultError({ msg : e })
     } else {
