68295491dcf881d972799db2e69c480dc9236ae2
Daniel Gomes <66312697+DanielGomesdaCunha@users.noreply.github.com>
2022-06-01 14:17:26 -0300

    Inserção Módulo Colaboradores

---
+++ backend/migrations/20220505003814_create_table_colaboradores.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('colaboradores', table => {
+      table.increments('id').primary()
+      table.string('nome').notNull()
+      table.string('codigo').notNull()
+      table.bigint('telefone1').notNull()
+      table.bigint('telefone2')
+      table.string('email').notNull()
+      table.string('competencia').notNull()
+      table.string('observacao')
+      table.integer('idcomum').references('id').inTable('comuns')
+      table.integer('idadministracao').references('id').inTable('administracoes')
+      table.integer('idregional').references('id').inTable('regionais')
+    })
+  };
+  
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTable('colaboradores')
+  };
+  
