d6aad35c1acf402c318888ed47f972f15fca25d9
Daniel Gomes <66312697+DanielGomesdaCunha@users.noreply.github.com>
2022-06-14 15:07:59 -0300

    Inserção - Módulo Surdos

---
+++ backend/migrations/20220610095733_create_table_surdos.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('surdos', table => {
+      table.increments('id').primary()
+      table.string('nome').notNull()
+      table.string('codigo').notNull()
+      table.string('telefone1').notNull()
+      table.string('telefone2')
+      table.string('batismo', 1).notNull()
+      table.string('observacao', 1000)
+      table.integer('idcomum').references('id').inTable('comuns')
+      table.integer('idadministracao').references('id').inTable('administracoes')
+      })
+};
+
+exports.down = function(knex, Promise) {
+  
+};
