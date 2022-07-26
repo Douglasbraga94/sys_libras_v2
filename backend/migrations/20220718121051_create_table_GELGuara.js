803a4e0e19799c039fcc424ad12a37bd8afb2d69
Daniel Gomes <66312697+DanielGomesdaCunha@users.noreply.github.com>
2022-07-26 10:45:16 -0300

    Inserção - Módulo Alunos

---
+++ backend/migrations/20220718121051_create_table_GELGuara.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('GELGuara', table => {
+      table.increments('id').primary()
+      table.string('nome').notNull()
+      table.string('codigo').notNull()
+      table.string('telefone1').notNull()
+      table.string('telefone2')
+      table.string('email').notNull()
+      table.string('surdo', 1).notNull()
+      table.string('status').notNull()
+      table.string('cartaEncaminhamento').notNull()
+      table.string('idadeInicioCurso')
+      table.date('dataBatismo').notNull()
+      table.date('dataNascimento').notNull()
+      table.string('observacao', 1000)
+      table.integer('idcomum').references('id').inTable('comuns')
+      table.integer('idadministracao').references('id').inTable('administracoes')
+      })
+  };
+  
+  exports.down = function(knex, Promise) {
+  
+  };
+  
