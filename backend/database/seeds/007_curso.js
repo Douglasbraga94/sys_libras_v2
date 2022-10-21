fdfa79729133c4e1c9f3421cae989161db0bbb21
André Luís Estevam Vieira <andre-estevam@users.noreply.github.com>
2022-10-20 22:00:46 -0300

    removido o atributo transmissaoVirtual do seed curso

--- backend/database/seeds/007_curso.js
+++ backend/database/seeds/007_curso.js
@@ -6,9 +6,9 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('curso').del()
   await knex('curso').insert([
-    { codigo: 1, nome: 'Grupo de Ensino em LIBRAS', sigla: 'GEL', cargaHoraria: undefined, transmissaoVirtual: false, ativo: true  },
-    { codigo: 2, nome: 'Letramento', sigla: undefined, cargaHoraria: undefined, transmissaoVirtual: false, ativo: true  },
-    { codigo: 3, nome: 'Atualização', sigla: undefined, cargaHoraria: undefined, transmissaoVirtual: true, ativo: true  },
-    { codigo: 4, nome: 'Adaptação', sigla: undefined, cargaHoraria: undefined, transmissaoVirtual: false, ativo: true  }
+    { codigo: 1, nome: 'Grupo de Ensino em LIBRAS', sigla: 'GEL', cargaHoraria: undefined, ativo: true  },
+    { codigo: 2, nome: 'Letramento', sigla: undefined, cargaHoraria: undefined, ativo: true  },
+    { codigo: 3, nome: 'Atualização', sigla: undefined, cargaHoraria: undefined, ativo: true  },
+    { codigo: 4, nome: 'Adaptação', sigla: undefined, cargaHoraria: undefined, ativo: true  }
   ]);
 };
